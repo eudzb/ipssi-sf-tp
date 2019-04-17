2dc5d8fffadd580a39a346493544cf75b4d11f9e
matis <matisse.varlin@hotmail.fr>
2019-04-17 14:43:44 +0200

    Ajout de la nav-bar et la page home

---
+++ src/Controller/homeController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Utilisateur 1
+ * Date: 16/04/2019
+ * Time: 15:03
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class homeController extends AbstractController
+{
+    public function home() {
+        return $this->render("home.html.twig"); // futur page d'accueil
+    }
+}
