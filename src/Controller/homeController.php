4ab137640825755b3092f7e53ed8ad133657b19a
matis <matisse.varlin@hotmail.fr>
2019-04-18 12:49:38 +0200

    Ajout de : Liens vers BDD + affichage des articles

--- src/Controller/homeController.php
+++ src/Controller/homeController.php
@@ -7,11 +7,29 @@
  */
 
 namespace App\Controller;
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class homeController extends AbstractController
 {
     public function home() {
-        return $this->render("home.html.twig"); //page d'accueil
+        $repository = $this->getDoctrine()->getRepository(Article::class);
+        return $this->render('home.html.twig', ['articles' => $repository->findAll()]);
+        //return $this->render("home.html.twig"); //page d'accueil
     }
+/*
+    public function articleBDD(){
+
+        $repository = $this->getDoctrine()->getRepository(Player::class);
+        return $this->render('home.html.twig', ['players' => $repository->findAll()]);
+
+
+        /*$articles = $this->getDoctrine()
+            ->getRepository('AcmeStoreBundle:articles')
+            ->findAll();
+
+        return $this->render("AscavideoBundle:views:home.html.twig", array(
+            'articles' => $articles,
+        ));
+    }*/
 }
