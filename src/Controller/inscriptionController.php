5eaf00a6dc1064fd6891d16e824f414e5eef2e8b
matis <matisse.varlin@hotmail.fr>
2019-04-19 11:38:45 +0200

    Inscription terminé !

--- src/Controller/inscriptionController.php
+++ src/Controller/inscriptionController.php
@@ -8,17 +8,6 @@
 
 namespace App\Controller;
 
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-class inscriptionController extends AbstractController
-{
-    public function inscription() {
-    return $this->render("inscription.html.twig");
-}
-}
-/*
-namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Player;
 use App\Form\InscriptionType;
@@ -26,9 +15,11 @@ use App\Repository\PlayerRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 class inscriptionController extends AbstractController
 {
     public function inscription(Request $request): Response {
+
         $form = $this->createForm(InscriptionType::class);
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()) {
@@ -40,8 +31,9 @@ class inscriptionController extends AbstractController
                 'id' => $player->getId(),
             ]);
         }
+
         return $this->render("inscription.html.twig", [
             'InscriptionType' => $form->createView()
         ]);
     }
-}*/
+}
