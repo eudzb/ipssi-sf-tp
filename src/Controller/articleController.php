ab0d94949470c7f405fb64604e4a88b2834730e5
matis <matisse.varlin@hotmail.fr>
2019-04-19 10:34:43 +0200

    Mise à jour

--- src/Controller/articleController.php
+++ src/Controller/articleController.php
@@ -7,16 +7,14 @@
  */
 
 namespace App\Controller;
-
-
-use App\Entity\Article;
+use App\Entity\Commentaire;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class articleController extends AbstractController
 {
     public function article() {
-        $repository = $this->getDoctrine()->getRepository(Article::class);
-        return $this->render('article.html.twig', ['articles' => $repository->findAll()]);
+        $repository = $this->getDoctrine()->getRepository(Commentaire::class);
+        return $this->render('article.html.twig', ['commentaires' => $repository->findAll()]);
         //return $this->render("article.html.twig");
     }
 }
